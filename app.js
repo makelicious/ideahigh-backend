c731541f1493b02a3d2f468a9fc5df053a92e140
restodon <restodon@users.noreply.github.com>
2016-05-07 22:06:16 +0300

    Added files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// database settings
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = require('monk')('localhost:27017/proto');
+// routes
+var thoughts = require('./routes/thoughts');
+var users = require('./routes/users');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+});
+
+app.use('/thoughts', thoughts);
+app.use('/users', users);
+
+app.get('/', function(req, res) {
+  res.render('index',{
+    title: 'Chronomonology' });
+});
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+
+});
+
---
+++ routes/thoughts.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+
+
+
+
+router.route('/.json/')
+  .get(function(req, res) {
+    var db = req.db;
+    var collection = db.get('thoughts');
+    collection.find({},{}, function(e, docs) {
+      console.log(docs);
+      res.json(docs);
+    });
+  })
+  .post(function(req, res) {
+    var db = req.db;
+    var collection = db.get('thoughts');
+    collection.insert(req.body, function(err, docs){
+        res.send(docs);
+    });
+  });
+
+    router.route('/thoughtlist.json')
+    .get(function(req, res) {
+      var db = req.db;
+      var collection = db.get('thoughts');
+      collection.distinct("hashtags", function(e, docs) {
+        var allHashtags = docs;
+        allHashtags.sort();
+         res.json(allHashtags);
+      });
+    });
+router.route('/')
+  .get(function(req, res) {
+    res.render('hashtaglist', {
+      title: 'List of hashtags'
+    });
+  });
+
+  router.route('/:hashtags.json')
+  .get(function(req, res) {
+    var db = req.db;
+    var collection = db.get('thoughts');
+    var currentHashtagBranch = req.params.hashtags;
+    collection.find({"hashtags": currentHashtagBranch}, function(e, docs) {
+      console.log(docs);
+      res.json(docs);
+    });
+  });
+
+  router.route('/:hashtags')
+    .get(function(req, res) {
+      res.render('thoughts', {
+        title: 'yeah'
+    });
+  });
+
+  router.route('/:hashtags/associatedhashtags.json')
+    .get(function(req, res) {
+    var db = req.db;
+    var collection = db.get('thoughts');
+    var currentHashtagBranch = req.params.hashtags;
+    collection.find({"hashtags": currentHashtagBranch}, function(e, docs) {
+      var thoughts = docs;
+       var allHashtags = thoughts.map(function(thought) {
+        return thought.hashtags;
+       });
+       var allHashtagsFlattened = allHashtags.reduce(function(hashtag, thought) {
+        return hashtag.concat(thought);
+       }, []);
+
+       var associatedHashtags = [];
+
+       allHashtagsFlattened.forEach(function(hashtag) {
+        if(hashtag !== currentHashtagBranch && associatedHashtags.indexOf(hashtag) === -1) {
+          associatedHashtags.push(hashtag);
+          return hashtag;
+        }
+       });
+       associatedHashtags.sort();
+       res.json(associatedHashtags);
+      });
+  });
+
+  router.route('/deletethought/:id')
+    .delete(function(req, res) {
+      var db = req.db;
+      var collection = db.get('thoughts');
+      var userToDelete = req.params.id;
+      collection.remove({ '_id': userToDelete });
+    });
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
