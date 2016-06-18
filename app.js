cd2e287e0419252087ba7b4887bedbcfd1399296
Riku Rouvila <riku.rouvila@gmail.com>
2016-06-18 19:47:53 +0300

    remove routes we don't currently need, use different collections for different boards

--- app.js
+++ app.js
@@ -6,32 +6,19 @@ var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = require('monk')(process.env.MONGODB_URI || 'localhost:27017/proto');
+
 // routes
 var thoughts = require('./routes/thoughts');
-var users = require('./routes/users');
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
-
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-
 
 app.use(function(req, res, next) {
   req.db = db;
   next();
 });
 
-app.use('/thoughts', thoughts);
-app.use('/users', users);
-
-app.get('/', function(req, res) {
-  res.render('index',{
-    title: 'Chronomonology' });
-});
+app.use('/:board/thoughts', thoughts);
 
 const port = process.env.PORT || 3000;
 
